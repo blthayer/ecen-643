44fa04b2d51ffd7557c0e39fca16e6db5a1d2587
Brandon Thayer <blthayer@tamu.edu>
2019-03-18 13:20:34 -0500

    - analytical.py:     - Putting running code into a "main" method.     - New method for computing system capacity.     - Adding code for computing the loss of load probability.

--- analytical.py
+++ analytical.py
@@ -59,152 +59,217 @@ def p_from_r(r):
     return np.linalg.solve(b, c)
 
 
-########################################################################
-# GENERATORS
-########################################################################
-# Set failure and repair rates.
-# (0.1/day) * (1 day / 24 hours)
-g_fr = 0.1 / 24
+def compute_capacity(g12_cap, g3_cap, t_cap):
+    """Helper to compute system capacity to deliver load."""
+    return min(g12_cap, t_cap) + g3_cap
 
-# (1/8hr)
-g_rr = 1 / 8
 
-# G1 and G2 have four possible states.
-# 0: 1U, 2U
-# 1: 1D, 2U
-# 2: 1U, 2D
-# 3: 1D, 2D
-# Initialize transition rate matrix for G1 and G2.
-g12_r = np.zeros([4, 4])
-# Fill in matrix.
-# 0 --> 1 (failure)
-g12_r[0, 1] = g_fr
-# 0 --> 2 (failure)
-g12_r[0, 2] = g_fr
-# 1 --> 0 (repair)
-g12_r[1, 0] = g_rr
-# 1 --> 3 (failure)
-g12_r[1, 3] = g_fr
-# 2 --> 0 (repair)
-g12_r[2, 0] = g_rr
-# 2 --> 3 (failure)
-g12_r[2, 3] = g_fr
-# 3 --> 2 (repair)
-g12_r[3, 2] = g_rr
-# 3 --> 1 (repair)
-g12_r[3, 1] = g_rr
-# Fill diagonals with row sums.
-g12_r = fill_r_diag(g12_r)
+def main():
+    ####################################################################
+    # GENERATORS
+    ####################################################################
+    # Set failure and repair rates.
+    # (0.1/day) * (1 day / 24 hours)
+    g_fr = 0.1 / 24
 
-# Compute steady state probabilities.
-g12_p = p_from_r(g12_r)
+    # (1/8hr)
+    g_rr = 1 / 8
 
-# Get the two state transition matrix for generator 3.
-g3_r = get_two_state_r(fr=g_fr, rr=g_rr)
+    # G1 and G2 have four possible states.
+    # 0: 1U, 2U
+    # 1: 1D, 2U
+    # 2: 1U, 2D
+    # 3: 1D, 2D
+    # Initialize transition rate matrix for G1 and G2.
+    g12_r = np.zeros([4, 4])
+    # Fill in matrix.
+    # 0 --> 1 (failure)
+    g12_r[0, 1] = g_fr
+    # 0 --> 2 (failure)
+    g12_r[0, 2] = g_fr
+    # 1 --> 0 (repair)
+    g12_r[1, 0] = g_rr
+    # 1 --> 3 (failure)
+    g12_r[1, 3] = g_fr
+    # 2 --> 0 (repair)
+    g12_r[2, 0] = g_rr
+    # 2 --> 3 (failure)
+    g12_r[2, 3] = g_fr
+    # 3 --> 2 (repair)
+    g12_r[3, 2] = g_rr
+    # 3 --> 1 (repair)
+    g12_r[3, 1] = g_rr
+    # Fill diagonals with row sums.
+    g12_r = fill_r_diag(g12_r)
 
-# Compute steady state probabilities for generator 3.
-g3_p = p_from_r(g3_r)
+    # Compute steady state probabilities.
+    g12_p = p_from_r(g12_r)
 
-########################################################################
-# TRANSMISSION + WEATHER
-########################################################################
-# Transmission failure and repair rates for normal and adverse weather:
-# 10 / year * 1 year / 8760 hours
-t_fr_nw = 10 / 8760
-# 100 / year * 1 year / 8760 hours
-t_fr_aw = 100 / 8760
-# Repair rate is 1/8hrs
-t_rr = 1 / 8
+    # Manually assign capacities to each state.
+    g12_c = np.array([150, 75, 75, 0])
 
-# Weather rates (consider transition from normal to averse a "failure"):
-w_fr = 1/200
-w_rr = 1/20
+    # Get the two state transition matrix for generator 3.
+    g3_r = get_two_state_r(fr=g_fr, rr=g_rr)
 
-# The transmission lines themselves form a four state system. Combined
-# with the weather, it makes an eight state system.
-# State Num.    Component States    Weather
-# 0:            1U, 2U              N
-# 1:            1D, 2U              N
-# 2:            1U, 2D              N
-# 3:            1D, 2D              N
-# 4:            1U, 2U              A
-# 5:            1D, 2U              A
-# 6:            1U, 2D              A
-# 7:            1D, 2D              A
-#
-# Initialize the 8x8.
-tw_r = np.zeros([8, 8])
-# Fill the matrix by going state by state and placing rates OUT of the
-# state in question. Read diagram left to right, top to bottom.
-# TODO: Put diagram in repo?
-# First row of diagram:
-# 2
-tw_r[2, 0] = t_rr
-tw_r[2, 3] = t_fr_nw
-tw_r[2, 6] = w_fr
-# 0
-tw_r[0, 1] = t_fr_nw
-tw_r[0, 2] = t_fr_nw
-tw_r[0, 4] = w_fr
-# 1
-tw_r[1, 0] = t_rr
-tw_r[1, 3] = t_fr_nw
-tw_r[1, 5] = w_fr
-# 3
-tw_r[3, 1] = t_rr
-tw_r[3, 2] = t_rr
-tw_r[3, 7] = w_fr
-# Second row of diagram:
-# 6
-tw_r[6, 2] = w_rr
-tw_r[6, 4] = t_rr
-tw_r[6, 7] = t_fr_aw
-# 4
-tw_r[4, 0] = w_rr
-tw_r[4, 5] = t_fr_aw
-tw_r[4, 6] = t_fr_aw
-# 5
-tw_r[5, 1] = w_rr
-tw_r[5, 4] = t_rr
-tw_r[5, 7] = t_fr_aw
-# 7
-tw_r[7, 3] = w_rr
-tw_r[7, 5] = t_rr
-tw_r[7, 6] = t_rr
-# Fill diagonals.
-tw_r = fill_r_diag(tw_r)
+    # Compute steady state probabilities for generator 3.
+    g3_p = p_from_r(g3_r)
 
-# Compute steady state probabilities for transmission states.
-tw_p = p_from_r(tw_r)
+    # Manually assign capacities to each state.
+    g3_c = np.array([75, 0])
 
-########################################################################
-# DEMAND (LOAD)
-########################################################################
-# Model demand as 4 states:
-# 0: 60 MW, duration: 12 hours
-# 1: 105 MW, duration: 4 hours
-# 2: 205 MW, duration: 4 hours
-# 3: 105 MW, duration: 4 hours
-#
-# Note that I'm not combining the 105MW states to make it easy to
-# build the transition rate matrix.
-#
-# Note we're using d for demand instead of l for load, as it's easier to
-# read.
+    ####################################################################
+    # TRANSMISSION + WEATHER
+    ####################################################################
+    # Transmission failure and repair rates for normal and adverse
+    # weather:
+    # 10 / year * 1 year / 8760 hours
+    t_fr_nw = 10 / 8760
+    # 100 / year * 1 year / 8760 hours
+    t_fr_aw = 100 / 8760
+    # Repair rate is 1/8hrs
+    t_rr = 1 / 8
 
-# Initialize transition rate matrix for demand.
-d_r = np.zeros([4, 4])
-# 0 --> 1:
-d_r[0, 1] = 1 / 12
-# 1 --> 2:
-d_r[1, 2] = 1 / 4
-# 2 --> 3:
-d_r[2, 3] = 1 / 4
-# 3 --> 0:
-d_r[3, 0] = 1 / 4
-# Fill diagonals.
-d_r = fill_r_diag(d_r)
+    # Weather rates (consider transition from normal to averse a
+    # "failure"):
+    w_fr = 1/200
+    w_rr = 1/20
 
-# Compute steady state probabilities.
-d_p = p_from_r(d_r)
-pass
+    # The transmission lines themselves form a four state system.
+    # Combined with the weather, it makes an eight state system.
+    # State Num.    Component States    Weather
+    # 0:            1U, 2U              N
+    # 1:            1D, 2U              N
+    # 2:            1U, 2D              N
+    # 3:            1D, 2D              N
+    # 4:            1U, 2U              A
+    # 5:            1D, 2U              A
+    # 6:            1U, 2D              A
+    # 7:            1D, 2D              A
+    #
+    # Initialize the 8x8.
+    tw_r = np.zeros([8, 8])
+    # Fill the matrix by going state by state and placing rates OUT of
+    # the state in question. Read diagram left to right, top to bottom.
+    # TODO: Put diagram in repo?
+    # First row of diagram:
+    # 2
+    tw_r[2, 0] = t_rr
+    tw_r[2, 3] = t_fr_nw
+    tw_r[2, 6] = w_fr
+    # 0
+    tw_r[0, 1] = t_fr_nw
+    tw_r[0, 2] = t_fr_nw
+    tw_r[0, 4] = w_fr
+    # 1
+    tw_r[1, 0] = t_rr
+    tw_r[1, 3] = t_fr_nw
+    tw_r[1, 5] = w_fr
+    # 3
+    tw_r[3, 1] = t_rr
+    tw_r[3, 2] = t_rr
+    tw_r[3, 7] = w_fr
+    # Second row of diagram:
+    # 6
+    tw_r[6, 2] = w_rr
+    tw_r[6, 4] = t_rr
+    tw_r[6, 7] = t_fr_aw
+    # 4
+    tw_r[4, 0] = w_rr
+    tw_r[4, 5] = t_fr_aw
+    tw_r[4, 6] = t_fr_aw
+    # 5
+    tw_r[5, 1] = w_rr
+    tw_r[5, 4] = t_rr
+    tw_r[5, 7] = t_fr_aw
+    # 7
+    tw_r[7, 3] = w_rr
+    tw_r[7, 5] = t_rr
+    tw_r[7, 6] = t_rr
+    # Fill diagonals.
+    tw_r = fill_r_diag(tw_r)
+
+    # Compute steady state probabilities for transmission states.
+    tw_p = p_from_r(tw_r)
+
+    # Manually assign capacities to each state.
+    tw_c = np.array([200, 100, 100, 0, 200, 100, 100, 0])
+
+    ####################################################################
+    # DEMAND (LOAD)
+    ####################################################################
+    # Model demand as 4 states:
+    # 0: 60 MW, duration: 12 hours
+    # 1: 105 MW, duration: 4 hours
+    # 2: 205 MW, duration: 4 hours
+    # 3: 105 MW, duration: 4 hours
+    #
+    # Note that I'm not combining the 105MW states to make it easy to
+    # build the transition rate matrix.
+    #
+    # Note we're using d for demand instead of l for load, as it's
+    # easier to read.
+
+    # Initialize transition rate matrix for demand.
+    d_r = np.zeros([4, 4])
+    # 0 --> 1:
+    d_r[0, 1] = 1 / 12
+    # 1 --> 2:
+    d_r[1, 2] = 1 / 4
+    # 2 --> 3:
+    d_r[2, 3] = 1 / 4
+    # 3 --> 0:
+    d_r[3, 0] = 1 / 4
+    # Fill diagonals.
+    d_r = fill_r_diag(d_r)
+
+    # Compute steady state probabilities.
+    d_p = p_from_r(d_r)
+
+    # Manually assign demand for each state.
+    d_d = np.array([60, 105, 205, 105])
+
+    ####################################################################
+    # COMPUTE LOSS OF LOAD PROBABILITY
+    ####################################################################
+    # Do some nasty, hard-coded nested for-loops to get all possible
+    # system states, their probabilities, and whether or not demand
+    # is served.
+
+    # Initialize failure probability.
+    # TODO: Create a table of all system states?
+    lolp = 0
+
+    # Loop over g12.
+    for g12_idx in range(len(g12_c)):
+
+        # Loop over g3.
+        for g3_idx in range(len(g3_c)):
+
+            # Loop over transmission + weather.
+            for tw_idx in range(len(tw_c)):
+
+                # Compute system capacity to serve demand.
+                system_capacity = \
+                    compute_capacity(g12_cap=g12_c[g12_idx],
+                                     g3_cap=g3_c[g3_idx],
+                                     t_cap=tw_c[tw_idx])
+
+                # Loop over demand.
+                for d_idx in range(len(d_d)):
+
+                    # Determine whether or not demand is met.
+                    demand_met = system_capacity >= d_d[d_idx]
+
+                    if not demand_met:
+                        # Compute the probability of being in this
+                        # system state.
+                        p = (g12_p[g12_idx, 0] * g3_p[g3_idx, 0]
+                             * tw_p[tw_idx, 0] * d_p[d_idx, 0])
+
+                        lolp += p
+
+    print('Loss of Load Probability: {:.4f}'.format(lolp))
+
+
+if __name__ == '__main__':
+    main()
